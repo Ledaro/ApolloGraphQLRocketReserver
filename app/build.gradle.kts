56ff48c87f4549c2758912915f50656c8259eb92
Ledaro <fzelaskowski@wp.pl>
2023-03-03 13:47:53 +0100

    configure Kotlin Gradle with Apollo and GraphQL Schema

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("com.android.application")
+    id("com.apollographql.apollo3").version("3.7.4")
+    kotlin("android")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "com.example.graphqlcountriesapp"
+        minSdk = 27
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Apollo
+    implementation("com.apollographql.apollo3:apollo-runtime:3.7.4")
+}
+
+apollo {
+    service("service") {
+        packageName.set("com.example.rocketreserver")
+    }
+}

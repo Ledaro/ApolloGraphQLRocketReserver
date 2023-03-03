7b19a67b9a1f200255597895f7160d7f8a2aabfc
Ledaro <fzelaskowski@wp.pl>
2023-03-03 15:40:16 +0100

    add Apollo Client and make first log

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     id("com.android.application")
+    id("androidx.navigation.safeargs")
     id("com.apollographql.apollo3").version("3.7.4")
     kotlin("android")
 }
 
 android {
-    compileSdk = 32
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.example.graphqlcountriesapp"
@@ -26,6 +27,11 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -40,12 +46,20 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation("androidx.paging:paging-runtime-ktx:3.1.1")
+    implementation("androidx.security:security-crypto:1.1.0-alpha04")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     //Apollo
     implementation("com.apollographql.apollo3:apollo-runtime:3.7.4")
+
+    //Coil
+    implementation("io.coil-kt:coil:1.4.0")
 }
 
 apollo {
--- app/src/main/java/com/example/apollographqlrocketreserver/MainActivity.kt
+++ app/src/main/java/com/example/apollographqlrocketreserver/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.apollographqlrocketreserver
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.rocketreserver.LaunchListQuery
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        lifecycleScope.launchWhenResumed {
+            val response = apolloClient.query(LaunchListQuery()).execute()
+
+            Log.d("LaunchList", "Success ${response.data}")
+        }
     }
 }
